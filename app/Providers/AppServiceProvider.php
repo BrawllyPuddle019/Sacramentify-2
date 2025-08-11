3f2b0bc5716ef1e42d84235b7eaedab55c0fc1c6
BrawllyPuddle019 <superman.adrian.agm@gmail.com>
2025-08-10 19:26:02 -0600

    Fix https mixed content: trusted proxies and forceScheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
 
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Restaurado: sin forzar https ni cambiar SameSite en local
+    if ($this->app->environment('production')) {
+        URL::forceScheme('https');
     }
+ }   
 }
